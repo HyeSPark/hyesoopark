5cb90b1fdc01cc6421dbffd1b989ee57399a7cf1
HyeSPark <80015199+HyeSPark@users.noreply.github.com>
2024-09-22 03:22:13 +0900

    update: 0922

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,79 +1,61 @@
-
 import { allProjects } from "@/.contentlayer/generated";
-import Image from "next/image"
+import Image from "next/image";
 import Header from "@/components/header";
 import Link from "next/link";
 
 export default function Home() {
-    function formatDate(date: string) {
-        return new Date(date).toLocaleDateString("kr-KR", {
-            year: "numeric",
-            month: "short",
-        });
-    }
-    // Sort projects by date (descending)
-    const projects = allProjects.sort((a, b) => (a.from > b.from ? -1 : 1));
+  function formatDate(date: string) {
+    return new Date(date).toLocaleDateString("kr-KR", {
+      year: "numeric",
+      month: "short",
+    });
+  }
+  // Sort projects by date (descending)
+  const projects = allProjects.sort((a, b) => (a.from > b.from ? -1 : 1));
 
-    return (
-        <div className="prose dark:prose-invert flex flex-wrap gap-4 max-w-4xl divide-y md:divide-y-0 ">
-            {allProjects.map((project) => (
-                <Link 
-                    key={project._id} 
-                    href={project.slug}
-                    className="relative flex flex-col justify-center gap-4 no-underline pt-6 md:pt-0 overflow-hidden md:rounded-md"
-                >
-                    <Image 
-                        alt={`The representative image of ${project.title}`} 
-                        src={project.thumb}
-                        width={400}
-                        height={240}
-                        className="m-0 bg-white rounded-md border"
-                    ></Image>
-                    <div className="md:absolute relative top-0 w-full h-full 
+  return (
+    <div className="prose dark:prose-invert flex flex-wrap gap-4 max-w-4xl divide-y md:divide-y-0 ">
+      {allProjects.map((project) => (
+        <Link
+          key={project._id}
+          href={project.slug}
+          className="relative flex flex-col justify-center gap-4 no-underline pt-6 md:pt-0 overflow-hidden md:rounded-md"
+        >
+          <Image
+            alt={`The representative image of ${project.title}`}
+            src={project.thumb}
+            width={400}
+            height={240}
+            className="m-0 bg-white rounded-md border"
+          ></Image>
+          <div
+            className="md:absolute relative top-0 w-full h-full 
                         md:opacity-0 hover:opacity-100 transition duration-500 
-                        flex flex-col gap-2 md:items-center justify-center md:px-5">
-                        <div className="absolute top-0 w-full h-full bg-black md:opacity-50 opacity-0"> </div>
-                        <div className="flex gap-2 z-20">
-                            {project.tags && project.tags.map((el, i) => (
-                                <kbd key={i} className="bg-main_color text-white px-2">#{el}</kbd>
-                            ))}
-                        </div>
-                        <div className="text-2xl font-bold md:text-white z-20">{project.title}</div>
-                        {project.description && (
-                            <div className="z-20 md:text-white">{project.description}</div>
-                        )}
-                        <h4 className="m-0 p-0 md:text-white z-20">
-                            {formatDate(project.from)} ~ {formatDate(project.to)}
-                        </h4>
-                    </div>
-                    {/* <div className="flex items-center justify-between w-full"> */}
-                        {/* <div className="flex-1">
-                            <Link href={project.slug}>
-                                <h2>{project.title}</h2>
-                            </Link>
-                            {project.description && (
-                                <p>{project.description}</p>
-                            )}
-                        </div> */}
-                        {/* Vertical seperator */}
-                        {/* <div className="mx-2.5" />
-                        <div className="w-44 flex flex-col items-center justify-center">
-                            {project.organization && (
-                                <h5>{project.organization}</h5>
-                            )}
-                            <h4 className="m-0 p-0 flex flex-col md:flex-row gap-1">
-                                <span>
-                                    {formatDate(project.from)} ~ 
-                                </span>
-                                <span>
-                                    {formatDate(project.to)}
-                                </span>
-                                
-                            </h4>
-                        </div> */}
-                    {/* </div> */}
-                </Link>
-            ))}
-        </div>
-    );
+                        flex flex-col gap-2 md:items-center justify-center md:px-5"
+          >
+            <div className="absolute top-0 w-full h-full bg-black md:opacity-50 opacity-0">
+              {" "}
+            </div>
+            <div className="flex gap-2 z-20">
+              {project.tags &&
+                project.tags.map((el, i) => (
+                  <kbd key={i} className="bg-main_color text-white px-2">
+                    #{el}
+                  </kbd>
+                ))}
+            </div>
+            <div className="text-2xl font-bold md:text-white z-20">
+              {project.title}
+            </div>
+            {project.description && (
+              <div className="z-20 md:text-white">{project.description}</div>
+            )}
+            <h4 className="m-0 p-0 md:text-white z-20">
+              {formatDate(project.from)} ~ {formatDate(project.to)}
+            </h4>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
--- app/publications/page.tsx
+++ app/publications/page.tsx
@@ -1,66 +1,104 @@
-
 import { Publication, allPublications } from "@/.contentlayer/generated";
-import {Link as LinkIcon, Video, Pdf} from "@/components/icons";
+import { Link as LinkIcon, Video, Pdf } from "@/components/icons";
 import Link from "next/link";
 import { ReactNode } from "react";
+import Image from "next/image";
 
 export default function Home() {
-    // Sort projects by date (descending)
-    const pubs = allPublications.sort((a, b) => (a.date > b.date ? -1 : 1));
+  // Sort projects by date (descending)
+  const pubs = allPublications.sort((a, b) => (a.date > b.date ? -1 : 1));
 
-    const fullPubs = pubs.filter((el) => el.isFullPaper);
-    const notFullPubs = pubs.filter((el) => !el.isFullPaper);
+  const fullPubs = pubs.filter((el) => el.isFullPaper);
+  const notFullPubs = pubs.filter((el) => !el.isFullPaper);
 
-    const renderLinkButton = (link: string, title: string, icon: ReactNode) => (
-        <Link href={link} 
-            target="_blank" 
-            className="text-sm py-1 px-2 rounded-sm bg-[#eeeeee] 
+  const renderLinkButton = (link: string, title: string, icon: ReactNode) => (
+    <Link
+      href={link}
+      target="_blank"
+      className="text-sm py-1 px-2 rounded-sm bg-[#eeeeee] 
             font-semibold text-[#494949] hover:bg-main_color hover:text-white 
             transition duration-200 no-underline flex gap-1 items-center
-            dark:bg-[#4d4d4d] dark:text-white dark:hover:text-[#494949] dark:hover:bg-main_color_dark">
-                {icon}
-                {title}
-            </Link>
-    )
+            dark:bg-[#4d4d4d] dark:text-white dark:hover:text-[#494949] dark:hover:bg-main_color_dark"
+    >
+      {icon}
+      {title}
+    </Link>
+  );
 
-
-    const renderPubList = (pub: Publication) => (
-        <article key={pub._id}>
-            <div className="flex flex-col md:flex-row md:gap-8 items-center justify-between w-full">
-                <img 
-                    className="w-full md:max-w-[250px] m-0 p-0.5 rounded-md border h-full object-cover mt-8 bg-white"
-                    src={pub.img}
-                ></img>
-                <div className="flex flex-col gap-1">
-                    <h3>{pub.title}</h3>
-                    <div>
-                        {pub.authors.map((auth, i) => (<span key={i} className={`font-semibold ${
-                            auth === "Hye Soo Park" ? "text-main_color dark:text-main_color_dark" : ""
-                        }`}>{auth}{i === pub.authors.length - 1 ? null : ", "}
-                        </span>))}
-                    </div>
-                    <div className="flex gap-1">
-                        {pub.venue} 
-                        {pub.venueLink && <>{" ("} <Link href={pub.venueLink} target="_blank">Link</Link> {")"}</>}
-                        
-                    </div>
-                    <div className="flex gap-2 py-1">
-                        {pub.pdfLink && renderLinkButton(pub.pdfLink, "PDF", <Pdf width={16} height={16}/>)}
-                        {pub.doiLink && renderLinkButton(pub.doiLink, "DOI", <LinkIcon width={16} height={16}/>)}
-                        {pub.vidLink && renderLinkButton(pub.vidLink, "VIDEO", <Video width={16} height={16}/>)}
-                    </div>
-                </div>
-            </div>
-        </article>
-    )
-
-    return (
-        <div className="!max-w-full prose dark:prose-invert">
-            <h2>Conference & Journal Papers</h2>
-            {fullPubs.map(renderPubList)}
-            <hr></hr>
-            <h2>Posters & Workshop Papers</h2>
-            {notFullPubs.map(renderPubList)}
+  const renderPubList = (pub: Publication) => (
+    <article key={pub._id}>
+      <div className="flex flex-col md:flex-row md:gap-8 items-center justify-between w-full">
+        <Image
+          className="w-full md:max-w-[250px] m-0 p-0.5 rounded-md border h-full object-cover mt-8 bg-white"
+          src={pub.img}
+          width={150}
+          height={150}
+          alt={pub.title}
+        ></Image>
+        <div className="flex flex-col gap-1">
+          <h3>{pub.title}</h3>
+          <div>
+            {pub.authors.map((auth, i) => (
+              <span
+                key={i}
+                className={`font-semibold ${
+                  auth === "Hye Soo Park"
+                    ? "text-main_color dark:text-main_color_dark"
+                    : ""
+                }`}
+              >
+                {auth}
+                {i === pub.authors.length - 1 ? null : ", "}
+              </span>
+            ))}
+          </div>
+          <div className="flex gap-1">
+            {pub.venue}
+            {pub.venueLink && (
+              <>
+                {" ("}{" "}
+                <Link href={pub.venueLink} target="_blank">
+                  Link
+                </Link>{" "}
+                {")"}
+              </>
+            )}
+          </div>
+          <div className="flex gap-2 py-1">
+            {pub.pdfLink &&
+              renderLinkButton(
+                pub.pdfLink,
+                "PDF",
+                <Pdf width={16} height={16} />
+              )}
+            {pub.doiLink &&
+              renderLinkButton(
+                pub.doiLink,
+                "DOI",
+                <LinkIcon width={16} height={16} />
+              )}
+            {pub.vidLink &&
+              renderLinkButton(
+                pub.vidLink,
+                "VIDEO",
+                <Video width={16} height={16} />
+              )}
+            {!pub.pdfLink && !pub.doiLink && !pub.vidLink && (
+              <span className="italic">(Under Review)</span>
+            )}
+          </div>
         </div>
-    );
+      </div>
+    </article>
+  );
+
+  return (
+    <div className="!max-w-full prose dark:prose-invert">
+      <h2>Conference & Journal Papers</h2>
+      {fullPubs.map(renderPubList)}
+      <hr></hr>
+      <h2>Posters & Workshop Papers</h2>
+      {notFullPubs.map(renderPubList)}
+    </div>
+  );
 }
